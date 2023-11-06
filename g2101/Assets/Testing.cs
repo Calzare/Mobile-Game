d3a6ccf09a08f28331b53d074e961313d7375ea9
Calzare <90734036+Calzare@users.noreply.github.com>
2023-11-06 12:09:31 +0000

    1
    Simple Sprites and attempt at Movement

---
+++ g2101/Assets/Testing.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public GameObject Player;
+    public bool LeftClick = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { if (LeftClick == true)
+        {
+            Player.transform.Translate(Vector2.right * Time.deltaTime);
+        }
+    }
+}
